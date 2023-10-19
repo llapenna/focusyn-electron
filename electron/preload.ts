642808c07d704828a8c4ceb19e5f9293f0c26602
Luciano Lapenna <lucianoglapenna@gmail.com>
2023-10-18 22:57:54 -0300

    feat: expose active window methdos in renderer

--- electron/preload.ts
+++ electron/preload.ts
@@ -1,10 +1,18 @@
-import { contextBridge } from 'electron';
+import { contextBridge, ipcRenderer } from 'electron';
 
 /**
- * Expose protected methods that allow the renderer process to use
+ * Expose protected methods that allow the renderer process to use node modules and methods
  */
 contextBridge.exposeInMainWorld('versions', {
   node: () => process.versions.node,
   chrome: () => process.versions.chrome,
   electron: () => process.versions.electron,
+});
+
+/**
+ * Expose active window related methods
+ */
+contextBridge.exposeInMainWorld('activeWindow', {
+  current: () => ipcRenderer.invoke('GET_CURRENT_WINDOW'),
+  all: () => ipcRenderer.invoke('GET_ALL_WINDOWS'),
 });
