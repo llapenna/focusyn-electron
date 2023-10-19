0cc3e6726f5b202221c08f7999af95a23ef28f3e
Luciano Lapenna <lucianoglapenna@gmail.com>
2023-10-18 22:30:29 -0300

    feat: add active window lib interface

--- electron/config.ts
+++ electron/config.ts
@@ -1,4 +1,5 @@
 import { join } from 'node:path';
+import isdev from 'electron-is-dev';
 
 // All these directories are relative to '<projectRoot>/dist-electron' folder
 
@@ -11,3 +12,5 @@ export const DIST_ELECTRON = __dirname;
 
 // Vite environment variables
 export const URL = process.env['VITE_DEV_SERVER_URL'];
+
+export const IS_DEV = isdev;
