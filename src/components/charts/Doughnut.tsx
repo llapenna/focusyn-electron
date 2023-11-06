ee528eb6fc70b26ca5d9adea0cc9cd738a050874
Luciano Lapenna <lucianoglapenna@gmail.com>
2023-11-06 20:46:20 -0300

    feat: update data structures in doughnut chart

--- src/components/charts/Doughnut.tsx
+++ src/components/charts/Doughnut.tsx
@@ -9,18 +9,19 @@ import { Doughnut as DoughnutChart } from 'react-chartjs-2';
 
 import { getFormattedTime } from '@/reactapp/utils/time';
 import { segToMs } from '@/shared/time';
+import { ActiveWindow } from '@/shared/types/activeWindow';
 
-import { dataToDataset } from './utils';
+import dataToDataset from './utils';
 
 ChartJS.register(ArcElement, Tooltip, Legend);
 
 interface Props {
-  data: Record<string, number>;
+  data: ActiveWindow.Grouped[];
 }
 
 export const Doughnut = ({ data }: Props) => {
-  const formattedData = dataToDataset(data, 'doughnut');
-  const options: ChartOptions = {
+  const formattedData = dataToDataset.doughnut(data);
+  const options: ChartOptions<'doughnut'> = {
     plugins: {
       tooltip: {
         enabled: true,
