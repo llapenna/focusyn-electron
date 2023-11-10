d75969217f76206f23696800c28763b33635e621
Luciano Lapenna <lucianoglapenna@gmail.com>
2023-11-10 19:23:07 -0300

    feat: increase generic container padding

--- src/components/layout/Container/styles.ts
+++ src/components/layout/Container/styles.ts
@@ -6,19 +6,19 @@ export const container = cva({
     flexDirection: 'column',
     gap: 'sm',
 
-    padding: 'sm',
-
     backgroundColor: 'overlay',
   },
   variants: {
     type: {
       main: {
+        padding: 'lg',
         borderWidth: '1px',
         borderColor: 'overlayBorder',
         borderStyle: 'solid',
         borderRadius: 'md',
       },
       subtle: {
+        padding: 'sm',
         borderWidth: '0.5px',
         borderColor: 'subtleBorder',
         borderStyle: 'solid',
