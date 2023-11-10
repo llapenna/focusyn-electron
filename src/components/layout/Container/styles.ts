4d9b8b85e69567756c97cfa1af5bf3324e92bc3c
Luciano Lapenna <lucianoglapenna@gmail.com>
2023-11-09 22:52:45 -0300

    feat: add generic Container style variant

--- src/components/layout/Container/styles.ts
+++ src/components/layout/Container/styles.ts
@@ -1,18 +1,34 @@
-import { cva } from "@/reactapp/styled/css";
+import { RecipeVariantProps, cva } from '@/reactapp/styled/css';
 
 export const container = cva({
   base: {
-    display: "flex",
-    flexDirection: "column",
-    gap: "sm",
+    display: 'flex',
+    flexDirection: 'column',
+    gap: 'sm',
 
-    padding: "sm",
+    padding: 'sm',
 
-    borderWidth: "1px",
-    borderColor: "overlayBorder",
-    borderStyle: "solid",
-    borderRadius: "md",
-
-    backgroundColor: "overlay",
+    backgroundColor: 'overlay',
+  },
+  variants: {
+    type: {
+      main: {
+        borderWidth: '1px',
+        borderColor: 'overlayBorder',
+        borderStyle: 'solid',
+        borderRadius: 'md',
+      },
+      subtle: {
+        borderWidth: '0.5px',
+        borderColor: 'subtleBorder',
+        borderStyle: 'solid',
+        borderRadius: 'md',
+      },
+    },
+  },
+  defaultVariants: {
+    type: 'main',
   },
 });
+
+export type ContainerVariants = RecipeVariantProps<typeof container>;
