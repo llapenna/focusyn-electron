e5b77161cde6b4ecd0ef64fdfc51d060277d7735
Luciano Lapenna <lucianoglapenna@gmail.com>
2023-11-10 22:39:03 -0300

    chore: remove old chart container file

--- src/components/layout/ChartContainer/index.tsx
+++
@@ -1,8 +0,0 @@
-interface Props {
-  children: React.ReactNode;
-}
-const ChartContainer: React.FC<Props> = ({ children }) => {
-  return <div>{children}</div>;
-};
-
-export default ChartContainer;
