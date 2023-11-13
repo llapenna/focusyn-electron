879a268558761a985ecb2edd30079c6787e87519
Luciano Lapenna <lucianoglapenna@gmail.com>
2023-11-12 23:25:50 -0300

    feat: refactor formatted time function by splitting code

--- src/utils/time.ts
+++ src/utils/time.ts
@@ -1,4 +1,5 @@
 import { secToMs } from '@/shared/time';
+import { TimeUnits } from '@/reactapp/types/time';
 
 /**
  * Helper function to convert a number to a `Date` object.
@@ -11,14 +12,14 @@ const numberToDate = (time: number, format: 'ms' | 's'): Date => {
 };
 
 /**
- * Generates a string in the format of "hh:mm:ss" from a Date object or milliseconds.
- * @param time Date object or milliseconds to generate a Date object from.
- * @returns A string in the format of "hh:mm:ss".
+ * Returns the hours, minutes and seconds of a Date object as numbers.
+ * @param date Date object
+ * @returns An object containing the hours, minutes and seconds of the Date object as numbers.
  */
-export const getFormattedTime = (
+export const getTimeUnits = (
   time: Date | number,
   format: 'ms' | 's' = 'ms'
-): string => {
+): TimeUnits => {
   const date = typeof time === 'number' ? numberToDate(time, format) : time;
 
   // Where to trim the ISO Date string. Example: 1970-01-01T/00:00:50/.000Z
@@ -29,6 +30,21 @@ export const getFormattedTime = (
     .toISOString()
     .slice(STRING_START, STRING_END)
     .split(':');
+
+  return { h: Number(h), m: Number(m), s: Number(s) };
+};
+
+/**
+ * Generates a string in the format of "hh:mm:ss" from a Date object or milliseconds.
+ * @param time Date object or milliseconds to generate a Date object from.
+ * @param format Whether the time is in milliseconds or seconds. Defaults to milliseconds.
+ * @returns A string in the format of "hh:mm:ss".
+ */
+export const getFormattedTime = (
+  time: Date | number,
+  format: 'ms' | 's' = 'ms'
+): string => {
+  const { h, m, s } = getTimeUnits(time, format);
 
   let result = `${s}s`;
 
