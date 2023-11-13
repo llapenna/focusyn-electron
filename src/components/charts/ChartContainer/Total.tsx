fb41c8a1ca7b5dc22ad56736f7645b5edf0b5f14
Luciano Lapenna <lucianoglapenna@gmail.com>
2023-11-13 00:23:17 -0300

    feat: create time unit component

--- src/components/charts/ChartContainer/Total.tsx
+++ src/components/charts/ChartContainer/Total.tsx
@@ -1,12 +1,40 @@
 import { Container } from '@/reactapp/components/layout';
+import type { TimeUnits } from '@/reactapp/types/time';
+
+import { total } from './styles';
+
+interface UnitProps {
+  value: number;
+  unit: string;
+}
+
+const Unit: React.FC<UnitProps> = ({ value, unit }) => {
+  const classes = total();
+  return (
+    <>
+      <span className={classes.value}>{value}</span>
+      <span className={classes.timeUnit}>{unit}</span>
+    </>
+  );
+};
 
 interface Props {
   title: string;
-  value: string;
+  value: TimeUnits;
 }
-export const Total: React.FC<Props> = ({ title, value }) => (
-  <Container type="subtle">
-    <p>{title}</p>
-    <p>{value}</p>
-  </Container>
-);
+export const Total: React.FC<Props> = ({ title, value }) => {
+  const classes = total();
+
+  return (
+    <Container type="subtle">
+      <div>
+        <p className={classes.title}>{title}</p>
+        <p>
+          <Unit value={value.h} unit="H" />
+          <Unit value={value.m} unit="M" />
+          <Unit value={value.s} unit="S" />
+        </p>
+      </div>
+    </Container>
+  );
+};
