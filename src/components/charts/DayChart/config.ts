3f439bde30e24067869d237e3d6aad0856dfc223
Luciano Lapenna <lucianoglapenna@gmail.com>
2024-01-26 19:02:57 -0300

    feat(daychart): add tick values generator

--- src/components/charts/DayChart/config.ts
+++ src/components/charts/DayChart/config.ts
@@ -1,3 +1,5 @@
+import range from 'lodash-es/range';
+
 export const sizes = {
   h: 150,
   w: '100%',
@@ -9,6 +11,13 @@ export const margins = 20;
 export const chart = {
   // 60 minutes/h * 24h/day = 1440 minutes/day
   ticks: 60 * 24,
+  tickValues() {
+    // Total number of ticks
+    const nTicks = 8;
+    const tickWidth = this.ticks / nTicks;
+
+    return range(0, this.ticks + tickWidth, tickWidth);
+  },
 
   step(width: number) {
     return width / this.ticks;
