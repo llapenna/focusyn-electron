a6ccbe0f809155cea1848e89213fa905d25a52d9
Luciano Lapenna <lucianoglapenna@gmail.com>
2024-01-29 01:56:03 -0300

    feat(daychart): improve chart config

--- src/components/charts/DayChart/config.ts
+++ src/components/charts/DayChart/config.ts
@@ -1,31 +1,45 @@
 import range from 'lodash-es/range';
 
-export const sizes = {
-  h: 150,
-  w: '100%',
-  axisH: 50,
-};
-
-export const margins = 20;
-
 export const chart = {
   // 60 minutes/h * 24h/day = 1440 minutes/day
-  ticks: 60 * 24,
-  tickValues() {
+  maxBarQty: 60 * 24,
+  size: {
+    h: 100,
+    width: '100%',
+  },
+  get tickValues() {
     // Total number of ticks
     const nTicks = 8;
-    const tickWidth = this.ticks / nTicks;
+    const tickWidth = this.maxBarQty / nTicks;
 
-    return range(0, this.ticks + tickWidth, tickWidth);
+    return range(0, this.maxBarQty + tickWidth, tickWidth);
   },
-
   step(width: number) {
-    return width / this.ticks;
+    return width / this.maxBarQty;
   },
   bounds: {
     x(width: number) {
-      return width - margins * 2;
+      return width - container.size.margin * 2;
     },
-    y: sizes.h - margins * 2,
+    get y() {
+      return container.size.h - container.size.margin * 2;
+    },
+  },
+};
+
+export const axis = {
+  size: {
+    h: 20,
+    w: '100%',
+  },
+};
+
+export const container = {
+  size: {
+    margin: 25,
+    get h(): number {
+      return chart.size.h + axis.size.h + this.margin * 2;
+    },
+    w: '100%',
   },
 };
--- src/components/charts/DayChart/styles.ts
+++ src/components/charts/DayChart/styles.ts
@@ -8,7 +8,7 @@ export const bar = cva({
   base: {
     transition: 'all 0.1s ease-in-out',
     '&:hover': {
-      transform: `translateY(-${container.margin - outlineWidth}px)`,
+      transform: `translateY(-${container.size.margin - outlineWidth}px)`,
       // total heigh) - outline * 3 (top, bottom and translate)
       height: container.size.h - outlineWidth * 3,
       outline: `${outlineWidth}px solid #F00`,
