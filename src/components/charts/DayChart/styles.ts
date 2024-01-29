0849a46721bddd6480bdba772701667e331c4098
Luciano Lapenna <lucianoglapenna@gmail.com>
2024-01-28 21:12:34 -0300

    feat(daychart): improve rect styles transition

--- src/components/charts/DayChart/styles.ts
+++ src/components/charts/DayChart/styles.ts
@@ -6,9 +6,10 @@ const outlineWidth = 2;
 // TODO: make the hover effect nicer
 export const bar = cva({
   base: {
-    transition: 'all 0.3s ease-in-out',
+    transition: 'all 0.1s ease-in-out',
     '&:hover': {
-      transform: `translateY(-18px)`,
+      transform: `translateY(-${container.margin - outlineWidth}px)`,
+      // total heigh) - outline * 3 (top, bottom and translate)
       height: container.size.h - outlineWidth * 3,
       outline: `${outlineWidth}px solid #F00`,
     },
