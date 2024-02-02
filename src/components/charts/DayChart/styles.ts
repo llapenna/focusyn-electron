42ec0cc07b2fcb421e3449e9a250041a0eb32b56
Luciano Lapenna <lucianoglapenna@gmail.com>
2024-02-02 20:27:15 -0300

    feat(daychart): add container styles

--- src/components/charts/DayChart/styles.ts
+++ src/components/charts/DayChart/styles.ts
@@ -1,16 +1,21 @@
 import { cva } from '@/reactapp/styled/css';
-import { container } from './config';
+import { container as containerConfig } from './config';
 
 const outlineWidth = 2;
 
-// TODO: make the hover effect nicer
+export const container = cva({
+  base: {
+    cursor: 'e-resize',
+  },
+});
 export const bar = cva({
   base: {
-    // transition: 'transform, outline, height 0.1s ease-in-out',
+    cursor: 'pointer',
+    // TODO: make the hover effect nicer
     '&:hover': {
-      transform: `translateY(-${container.size.margin - outlineWidth}px)`,
+      transform: `translateY(-${containerConfig.size.margin - outlineWidth}px)`,
       // total height - outline * 3 (top, bottom and translate)
-      height: container.size.h - outlineWidth * 3,
+      height: containerConfig.size.h - outlineWidth * 3,
       outline: `${outlineWidth}px solid #F00`,
     },
   },
