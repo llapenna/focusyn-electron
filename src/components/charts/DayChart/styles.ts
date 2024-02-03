d786ea4b05c31b2035c34bf4df821119eac846b3
Luciano Lapenna <lucianoglapenna@gmail.com>
2024-02-02 21:18:37 -0300

    chore(daychart): temporarely disable bar animation

--- src/components/charts/DayChart/styles.ts
+++ src/components/charts/DayChart/styles.ts
@@ -1,7 +1,4 @@
 import { cva } from '@/reactapp/styled/css';
-import { container as containerConfig } from './config';
-
-const outlineWidth = 2;
 
 export const container = cva({
   base: {
@@ -12,11 +9,11 @@ export const bar = cva({
   base: {
     cursor: 'pointer',
     // TODO: make the hover effect nicer
-    '&:hover': {
-      transform: `translateY(-${containerConfig.size.margin - outlineWidth}px)`,
-      // total height - outline * 3 (top, bottom and translate)
-      height: containerConfig.size.h - outlineWidth * 3,
-      outline: `${outlineWidth}px solid #F00`,
-    },
+    // '&:hover': {
+    //   transform: `translateY(-${containerConfig.size.margin - outlineWidth}px)`,
+    //   // total height - outline * 3 (top, bottom and translate)
+    //   height: containerConfig.size.h - outlineWidth * 3,
+    //   outline: `${outlineWidth}px solid #F00`,
+    // },
   },
 });
