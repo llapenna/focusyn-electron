e4cb26639662facc6d8486dec42f8fda6c6d0ce0
Luciano Lapenna <lucianoglapenna@gmail.com>
2024-02-03 22:10:51 -0300

    feat(daychart): add clamp option to useScale

--- src/components/charts/DayChart/useScale.ts
+++ src/components/charts/DayChart/useScale.ts
@@ -5,6 +5,7 @@ import { chart } from './config';
 interface Props {
   width: number;
   x?: [number, number];
+  clamp?: boolean;
 }
 /**
  * Generates a linear scale function to convert minutes to pixels and memoizes it
@@ -12,7 +13,11 @@ interface Props {
  * @param x Domain of the x axis (in minutes)
  * @returns A linear scale function to convert minutes to pixels
  */
-export const useScale = ({ width, x = [0, chart.maxBarQty] }: Props) => {
+export const useScale = ({
+  width,
+  x = [0, chart.maxBarQty],
+  clamp = true,
+}: Props) => {
   const [xMin, xMax] = x;
 
   const scale = useMemo(
@@ -20,9 +25,9 @@ export const useScale = ({ width, x = [0, chart.maxBarQty] }: Props) => {
       scaleLinear<number>({
         domain: [xMin, xMax],
         range: [0, chart.size.w(width)],
-        clamp: true,
+        clamp,
       }),
-    [width, xMax, xMin]
+    [width, xMax, xMin, clamp]
   );
 
   return scale;
