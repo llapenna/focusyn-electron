dabc9b2d6780fe18bc4b3dbaf0037e53c6ebe650
Luciano Lapenna <lucianoglapenna@gmail.com>
2024-02-09 02:34:11 -0300

    feat(overview): calculate totals

--- src/pages/Overview/index.tsx
+++ src/pages/Overview/index.tsx
@@ -1,19 +1,35 @@
 import { Alarm, Moon, Target } from '@phosphor-icons/react';
 
 import { hstack } from '@/reactapp/styled/patterns';
+import { useActiveWindows } from '@/reactapp/context/ActiveWindows';
 
 import { overview } from './styles';
 import { Total } from './Total';
 import { Chart } from './Chart';
+import { getTotalTime } from '@/reactapp/services/activeWindow/getTotalTime';
+import { getFormattedTime } from '@/reactapp/utils/time';
 
 const Overview = () => {
+  const windows = useActiveWindows();
+
+  const total = getTotalTime(windows, 'both');
+  const idle = getTotalTime(windows, 'idle');
+
   return (
     <div className={overview()}>
       <div className={hstack({ gap: 'xl' })}>
-        {/* TODO: set correct times */}
-        <Total title="Total" value="12h 15m" icon={<Alarm size={24} />}></Total>
-        <Total title="Focus" value="2h 30" icon={<Target size={24} />}></Total>
-        <Total title="Idle" value="30m" icon={<Moon size={24} />}></Total>
+        <Total
+          title="Total"
+          value={getFormattedTime(total)}
+          icon={<Alarm size={24} />}
+        ></Total>
+        {/* TODO: find a way to calculate focus time */}
+        {/* <Total title="Focus" value="2h 30" icon={<Target size={24} />}></Total> */}
+        <Total
+          title="Idle"
+          value={getFormattedTime(idle)}
+          icon={<Moon size={24} />}
+        ></Total>
       </div>
       <Chart />
     </div>
