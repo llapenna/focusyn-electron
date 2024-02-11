dc01d510fce67c0c3059bf82fae2f397e91b1902
Luciano Lapenna <lucianoglapenna@gmail.com>
2024-02-10 23:15:16 -0300

    fix(activeWindow): set correct filter for focus state

--- src/services/activeWindow/filterBy.ts
+++ src/services/activeWindow/filterBy.ts
@@ -12,13 +12,17 @@ export const filterBy = (
   include: FilterBy = 'All'
 ): Grouped[] => {
   switch (include) {
-    case 'All':
-      return arr;
     case 'Active':
       return arr.filter(({ owner }) => owner.name !== 'Idle');
     case 'Idle':
       return arr.filter(({ owner }) => owner.name === 'Idle');
     case 'Focus':
-      return arr.filter(({ group }) => group.count >= FOCUS_COUNT_THRESHOLD);
+      return arr.filter(
+        ({ group, owner }) =>
+          owner.name !== 'Idle' && group.count >= FOCUS_COUNT_THRESHOLD
+      );
+    case 'All':
+    default:
+      return arr;
   }
 };
